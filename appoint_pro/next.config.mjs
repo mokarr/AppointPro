1253e5d57fec9b54453895d0f043df61a41c72b2
Mokarr <mokarr246@gmail.com>
2025-04-28 21:07:37 +0200

    Next config

---
+++ appoint_pro/next.config.mjs
@@ -0,0 +1,20 @@
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // Only apply in the browser build
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                async_hooks: false, // Provide an empty module for async_hooks
+                fs: false,
+                net: false,
+                tls: false,
+                pg: false,
+            };
+        }
+
+        return config;
+    },
+    transpilePackages: ['@prisma/client', 'prisma']
+};
+
+export default nextConfig; 
