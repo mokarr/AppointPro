58052e719081ee14dbbb65d28b483bd526424899
Moka <mokarr246@gmail.com>
2025-05-25 14:59:39 +0200

    new prisma client

--- appoint_pro/src/lib/prisma-script.ts
+++ appoint_pro/src/lib/prisma-script.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '../../prisma/app/generated/prisma/client'
 
 // PrismaClient for scripts (doesn't use server-only restrictions)
 const prismaClient = new PrismaClient({
--- appoint_pro/src/lib/server/db.ts
+++ appoint_pro/src/lib/server/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../../../prisma/app/generated/prisma/client'
 import { ensureServerSide } from '@/lib/prisma'
 
 // Ensure this code only runs on the server
